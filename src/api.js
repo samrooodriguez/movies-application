402b710ced99b3f4bf24fab8df4d0f3e35576cbc
Sam Rodriguez <sam27rodriguez@gmail.com>
2019-01-06 21:15:10 -0600

    added a ton of functionality and built out the html page more all that is left to do is to toss what is in the movies array onto the html

--- src/api.js
+++ src/api.js
@@ -3,7 +3,38 @@ module.exports = {
         return fetch('/api/movies')
             .then(response => response.json());
     },
-         thisIsAtest: () => console.log("this is a test")
+    addMovie: (movie) => {
+        const options = {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(movie)
+        };
+        console.log(movie);
+        return fetch('/api/movies', options)
+            .then(response => response.json())
+    },
+    deleteMovie: (movie, id) => {
+        const options = {
+            method: "DELETE",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(movie)
+        };
+        console.log(movie);
+        return fetch(`/api/movies/${id}`, options)
+    },
+    editMovie: (movie, id) => {
+        const options = {
+            method: "PATCH",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(movie)
+        };
+        console.log(movie);
+        return fetch(`/api/movies/${id}`, options)
+    }
 };
-
-
--- src/index.js
+++ src/index.js
@@ -1,16 +1,36 @@
-/**
- * require style imports
- */
-const {getMovies, thisIsAtest} = require('./api.js');
+import {getMovies,addMovie,deleteMovie,editMovie} from'./api.js';
+const form = document.querySelector('form');
+const input = document.querySelector('#searchTerm');
+const movieTitle = document.querySelector('#movie-title');
+const movieRating = document.querySelector('#movie-rating');
+const addMoviebutton = document.querySelector('#add-movie');
 
-getMovies().then((movies) => {
-    console.log('Here are all the movies:');
-    movies.forEach(({title, rating, id}) => {
-        console.log(`id#${id} - ${title} - rating: ${rating}`);
+const movieRefresh = () => {
+    getMovies().then((movies) => {
+        console.log('Here are all the movies:');
+        movies.forEach(({title, rating, id}) => {
+            console.log(`id#${id} - ${title} - rating: ${rating}`);
+        })
+
+    }).catch((error) => {
+        alert('Oh no! Something went wrong.\nCheck the console for details.');
+        console.log(error);
     });
-
-}).catch((error) => {
-    alert('Oh no! Something went wrong.\nCheck the console for details.')
-    console.log(error);
+};
+addMoviebutton.addEventListener('click', (event) => {
+           event.preventDefault();
+           const addedMovietitle = movieTitle.value;
+           const addedMovieRating = movieRating.value;
+            const addedMovie = {
+                title: addedMovietitle,
+                rating: addedMovieRating
+            };
+            addMovie(addedMovie);
+            console.log(addedMovie);
+            movieRefresh();
 });
-thisIsAtest();
+
+
+
+
+
